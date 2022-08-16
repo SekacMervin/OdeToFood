70280c8fa56dbc683a4006b24c62943d1a115ad9
DARO <daro@dhigroup.com>
2022-08-16 14:00:02 +0200

    All data to repository

---
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        //[Range(1,4)]
+        public string Name { get; set; }
+
+        //[DisplayFormat(NullDisplayText =)]
+        [Display(Name ="Type of food")]
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,57 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+
+        private readonly OdeToFoodDbContext db;
+
+
+        // KOnstruktor
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+
+
+        public void Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var restaurant = db.Restaurants.Find(id);
+            db.Restaurants.Remove(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            // Jedna moznost
+            // return from r in db.Restaurants orderby r.Name select r;
+            return db.Restaurants.OrderBy(r => r.Name);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
